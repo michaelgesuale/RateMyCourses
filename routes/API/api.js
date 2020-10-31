c940f1e012d1d5ef1d0f82966c37ab7a3ee4f5ee
Simon <Simon@localhost>
2020-10-30 23:06:20 -0400

    Add API for posting reviews

--- routes/API/api.js
+++ routes/API/api.js
@@ -7,5 +7,8 @@ const bodyParser = require('body-parser');
 //API routes go here 
 router.route('/v1/course/:course_id')
     .get(db.getCourseInfo)
+    
+router.route('/v1/reviews')
+    .post(db.postReview)
 
 module.exports = router;
--- routes/API/queries.js
+++ routes/API/queries.js
@@ -54,3 +54,83 @@ exports.getCourseInfo = [
 	}).catch(e => {res.status(500); res.send(e)})
   }
 ];
+
+
+exports.postReview = [
+	body('course_id')
+	.exists()
+	.withMessage('Missing Course Id Parameter')
+	.bail()
+	.isInt()
+	.withMessage('Invalid Course Id Parameter')
+	.bail()
+	.escape(),
+	body('user_id')
+	.exists()
+	.withMessage('Missing User Id Parameter')
+	.bail()
+	.matches(/^[a-zA-Z0-9 ]+$/i)
+	.withMessage('Invalid User Id Parameter')
+	.bail()
+	.escape(),
+	body('user_comment')
+	.exists()
+	.withMessage('Missing User Comment Parameter')
+	.bail()
+	.escape(),
+	body('workload')
+	.exists()
+	.withMessage('Missing Workload Parameter')
+	.bail()
+	.isInt()
+	.withMessage('Invalid Workload Parameter')
+	.bail()
+	.escape(),
+	body('enjoyment')
+	.exists()
+	.withMessage('Missing Enjoyment Parameter')
+	.bail()
+	.isInt()
+	.withMessage('Invalid Enjoyment Parameter')
+	.bail()
+	.escape(),
+	body('difficulty')
+	.exists()
+	.withMessage('Missing Difficulty Parameter')
+	.bail()
+	.isInt()
+	.withMessage('Invalid Difficulty Parameter')
+	.bail()
+	.escape(),
+	body('usefulness')
+	.exists()
+	.withMessage('Missing Usefulness Parameter')
+	.bail()
+	.isInt()
+	.withMessage('Invalid Usefulness Parameter')
+	.bail()
+	.escape(),
+	async function(req, res, next) {
+		// First see if we have any errors
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			// If there are errors. We want to render form again with sanitized values/errors messages.
+			res.status(400).json({ errors: errors.array() });
+			return;
+		}
+		
+		db.task(async t => {	
+		overall = (parseInt(req.body.workload) + parseInt(req.body.enjoyment) + parseInt(req.body.difficulty) + parseInt(req.body.usefulness)) / 4
+
+		return await t.one(insertReview, [req.body.course_id , req.body.user_id, req.body.user_comment, req.body.workload, req.body.enjoyment, req.body.difficulty, req.body.usefulness, overall]);
+		
+	}).then (result => {
+     	   if ('review_id' in result) {
+          	res.status(200).end();
+       	 } else {
+		// return 400 if unsuccessful
+        	res.status(400).send('Unable to create the review');
+       	 }
+	}).catch(e => {res.status(500); console.log(e); res.send(e)})
+  }
+];
