efbdc053ba4a620cea80393a84f460d6ec13adc7
Simon <Simon@localhost>
2020-10-30 23:02:32 -0400

    Add API for getting course data

--- routes/API/api.js
+++ routes/API/api.js
@@ -2,11 +2,10 @@ var express = require('express');
 const router = express.Router({ mergeParams: true, strict: true });
 
 const db = require('./queries');
+const bodyParser = require('body-parser');
 
 //API routes go here 
-
-router.route('/v1/test')
-    .get(db.test)
-
+router.route('/v1/course/:course_id')
+    .get(db.getCourseInfo)
 
 module.exports = router;
--- routes/API/queries.js
+++ routes/API/queries.js
@@ -10,17 +10,47 @@ const {
 	validationResult
 } = require('express-validator');
 
+// DB Queries
+const getCourse = `SELECT * FROM courses WHERE course_id = $1`
+const getReviews = `SELECT * FROM reviews WHERE course_id = $1`
+const getPrereq = `SELECT * FROM prereq, courses WHERE prereq.course_id = $1 AND courses.course_id = prereq.require;`
+
+const insertReview = `INSERT INTO reviews(course_id, user_id, user_comment, workload, enjoyment, difficulty, usefulness, overall) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING review_id;`
+
 // Run Queries here 
-exports.test = [
-  async function(req, res, next) {
-	db.task(async t => {	
-		const result = await t.any(`SELECT * FROM university;`, []);
+exports.getCourseInfo = [
+	param('course_id')
+	.exists()
+	.withMessage('Missing Course Id Parameter')
+	.bail()
+	.isInt()
+	.withMessage('Invalid Course Id Parameter')
+	.bail()
+	.escape(),
+	async function(req, res, next) {
+		// First see if we have any errors
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			// If there are errors. We want to render form again with sanitized values/errors messages.
+			res.status(400).json({ errors: errors.array() });
+			return;
+		}
 		
+		db.task(async t => {	
+		const courseResult = await t.one(getCourse, [req.params.course_id]);
+		const reviewResults = await t.any(getReviews, [req.params.course_id]);
+		const prereqResults = await t.any(getPrereq, [req.params.course_id]);
+
+		
+		result = {
+			  course: courseResult,
+			  reviews: reviewResults,
+			  prereq: prereqResults
+			 }
+
 		return result
 	}).then (result => {
 		res.status(200).json(result);
 	}).catch(e => {res.status(500); res.send(e)})
-	
   }
 ];
-
