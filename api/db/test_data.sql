b58c95e5c9150f169456f501577f63cf9244ddee
Simon <Simon@localhost>
2020-11-18 14:54:11 -0500

    Fixed user error in insert data script

--- api/db/test_data.sql
+++ api/db/test_data.sql
@@ -8,6 +8,9 @@ INSERT INTO courses(name, campus, description, year, subject) VALUES ('MAT232', 
 INSERT INTO courses(name, campus, description, year, subject) VALUES ('CSC207', 1, 'CSC207 description',1, 'CS');
 
 INSERT INTO users(email, username, password) VALUES ('test@mail.utoronto.ca', 'test', '123456');
+INSERT INTO users(email, username, password) VALUES ('test1@mail.utoronto.ca', 'test1', '123456');
+INSERT INTO users(email, username, password) VALUES ('test2@mail.utoronto.ca', 'test2', '123456');
+
 INSERT INTO users(email, username, password) VALUES ('newUser@mail.utoronto.ca', 'newUser', 'newUser');
 INSERT INTO users(email, username, password) VALUES ('newUser@mail.utoronto.com', 'newUserC', 'newUser');
 
@@ -19,8 +22,8 @@ INSERT INTO reviews(course_id, username, user_comment, workload, enjoyment, diff
 
 INSERT INTO reviews(course_id, username, user_comment, workload, enjoyment, difficulty, usefulness, overall) VALUES (4, 'test', 'CSC207 REVIEW', 5, 5, 5, 5, 5);
 
-INSERT INTO reviews(course_id, username, user_comment, workload, enjoyment, difficulty, usefulness, overall) VALUES (1, 'test', 'CSC490 REVIEW 2', 5, 5, 5, 5, 5);
-INSERT INTO reviews(course_id, username, user_comment, workload, enjoyment, difficulty, usefulness, overall) VALUES (1, 'test', 'CSC490 REVIEW 3', 3, 3, 3, 3, 3);
+INSERT INTO reviews(course_id, username, user_comment, workload, enjoyment, difficulty, usefulness, overall) VALUES (1, 'test1', 'CSC490 REVIEW 2', 5, 5, 5, 5, 5);
+INSERT INTO reviews(course_id, username, user_comment, workload, enjoyment, difficulty, usefulness, overall) VALUES (1, 'test2', 'CSC490 REVIEW 3', 3, 3, 3, 3, 3);
 
 INSERT INTO prereq(course_id, require) VALUES (1, 2);
 INSERT INTO prereq(course_id, require) VALUES (1, 4);
